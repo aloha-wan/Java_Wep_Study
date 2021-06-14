151c834f4df3080a4f61db302c60d9d8a1f5cdb4
DoWan Kim <50447511+aloha-wan@users.noreply.github.com>
2021-06-14 23:17:50 +0900

    java start

--- FAST_JAVA/Homework/src/ABClass/AClass.java
+++
@@ -1,6 +0,0 @@
-package ABClass;
-
-public class AClass extends BClass{
-
-	//The hierarchy of the type BClass is inconsistent
-}
--- FAST_JAVA/Homework/src/ABClass/BClass.java
+++
@@ -1,8 +0,0 @@
-package ABClass;
-
-public class BClass extends AClass {
-
-	//The hierarchy of the type BClass is inconsistent
-	
-	//Cycle detected: a cycle exists in the type hierarchy between BClass and AClass
-}
