92697b298b88682748d35f591be4139b0a5316d7
DoWan Kim <50447511+aloha-wan@users.noreply.github.com>
2021-07-02 03:46:14 +0900

    chapter02 변수선언하기

---
+++ FAST_JAVA/Chapter2/src/variable/VariableTest.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+
+		// 노란줄이 나온다면 변수를 선언하고 한번도 사용하지 않았다. 라는 뜻
+		int age;
+		
+		age = 30;
+		
+		System.out.println(age);
+		
+	}
+
+}
